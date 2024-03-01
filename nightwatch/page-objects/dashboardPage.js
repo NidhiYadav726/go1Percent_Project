a956ee72bb5473a340f3ed6a82adcb01e7901021
yadavnidhi0520 <nidhi.yadav@knoldus.com>
2024-03-01 17:54:53 +0530

    new commit

--- nightwatch/page-objects/dashboardPage.js
+++ nightwatch/page-objects/dashboardPage.js
@@ -2,32 +2,35 @@ module.exports = {
     url: 'https://nashtechglobal.qa.go1percent.com/',
 
     elements: {
-        activitiesBlock: {
-            selector: 'div.card-gap app-my-activity div.card.tab-card',
+        usernameInputField: {
+            selector: '#username',
             locatorStrategy: 'css selector',
+        },
+
+        passwordInputField: {
+            selector: '#password',
+            locatorStrategy: 'css selector',
+        },
+        loginButton: {
+            selector: '#kc-login',
+            locatorStrategy: 'css selector',
+        },
+        activitiesBlock: {
+            selector: '.timeline.w-100.mx-1',
+            locatorStrategy: 'css selector',
+        },
+        
     },
-},
 
 
     commands:[
     {
         successfulLogin : function(username,password) {
             return this
-                .setValue('#username', username)   //usernameInputField
-                .setValue('#password',password)    //passwordInputFIeld
-                .click('#kc-login')                //loginButton
-        },
-
-
-    
-        myActivity: function(){
-               browser.navigateTo('https://knoldus.qa.go1percent.com/my-profile')
-               
- 
-               
-                
+                .setValue('@usernameInputField', username)  
+                .setValue('@passwordInputField',password)    
+                .click('@loginButton')               
             },
         },
-
     ],
 }; 
--- test/dashboardActivityFunctionality.js
+++ test/dashboardActivityFunctionality.js
@@ -3,7 +3,7 @@ const dashboardPage = browser.page.dashboardPage();
 describe('website main elements', function() {
     before(function(browser) {
         const dashboard = browser.page.dashboardPage();
-       const login = browser.page.loginPage();
+        const login = browser.page.loginPage();
         dashboardPage.navigate();
         dashboardPage.maximizeWindow();
     });
@@ -13,14 +13,15 @@ it('Verify successful login with valid credentials', function(browser) {
     const login = browser.page.loginPage()
     login.successfulLogin('testadmin','testadmin');
     browser.assert.urlEquals('https://nashtechglobal.qa.go1percent.com/my-dashboard')
-    browser.navigateTo('https://knoldus.qa.go1percent.com/my-profile')
-     
-   
 });
 
-it('my activity', function(browser){
+it('Verify visibility of all the activities', async(browser)=>{
     const dashboard = browser.page.dashboardPage()
-    dashboard.myActivity();
-    browser.assert.urlEquals('https://knoldus.qa.go1percent.com/my-profile')
-   
+    await browser.execute('document.querySelector(".material-symbols-outlined.hex").click();');
+    browser.waitForElementVisible('@activitiesBlock',500,'wait for visibility')
+    browser.assert.visible('@activitiesBlock'," my activities are visible")
 });
+          
+
+
+
